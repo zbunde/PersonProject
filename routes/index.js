8431a201bd12dd237430a0a6f4da6f8b8275a365
marthaberner <marthaberner.mb@gmail.com>
2015-07-16 13:40:08 -0600

    jasmine wired up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Princeton Surveys' });
+  res.send("Hello World");
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/signup', function(req, res, next) {
-  res.render('users/signup');
+  res.send('Signup');
 });
 
 router.get('/signin', function(req, res, next) {
