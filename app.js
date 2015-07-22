33969602581401f4c6c5823868953f760b368c1b
marthaberner <marthaberner.mb@gmail.com>
2015-07-22 11:22:42 -0600

    use locals instead of cookie

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+app = express();
+var user = app.locals.user;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
