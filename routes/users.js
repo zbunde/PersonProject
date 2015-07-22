53cb9ee82bec71d5a1c4781f1c325defe1916cd8
marthaberner <marthaberner.mb@gmail.com>
2015-07-22 10:50:22 -0600

    a user can login

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var validUser = require('../lib/user_validation');
 
 /* GET users listing. */
+
+router.get('/', function(req, res, next) {
+  res.render('users/index', { user: req.cookies.username });
+});
+
 router.get('/signup', function(req, res, next) {
   res.render('users/signup');
 });
 
 router.get('/signin', function(req, res, next) {
   res.render('users/signin');
+});
+
+router.post('/signin', function(req, res, next) {
+  if(validUser(req.body)){
+    res.cookie("username", "user@example.com");
+    res.redirect('/users');
+  } else {
+    res.render("users/signin", { errors: "Invalid username/password" })
+  };
 });
 
 module.exports = router;
