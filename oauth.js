09f4a93424863eb29c2809cec4ddd43bbfb0b741
marthaberner <marthaberner.mb@gmail.com>
2015-08-05 14:54:15 -0600

    remove oath from gitignore

---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+facebook: {
+ clientID: process.env.CLIENT_ID,
+ clientSecret: process.env.FB_SECRET,
+ callbackURL: process.env.CALLBACK_URL,
+ enableProof: false
+  }
+}
+module.exports = ids
