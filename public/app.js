a153b50fceddbe6e0a285154ea24ce17dd32520a
marthaberner <marthaberner.mb@gmail.com>
2015-08-27 16:30:09 -0600

    users can signup

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
-var app = angular.module('person-project', ['ngRoute']);
+var app = angular.module('person-project', ['ngRoute', 'ngCookies']);
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
   $routeProvider
   .when('/', {
     templateUrl: "/partials/about.html",
@@ -10,23 +10,35 @@ app.config(function ($routeProvider) {
     templateUrl: "/partials/signup.html",
     controller: "SignupController"
   })
+  .when('/users/:id', {
+    templateUrl: "/partials/user.html",
+    controller: "UsersController"
+  })
   .when('/users', {
     templateUrl: "/partials/users.html",
     controller: "UsersController"
   }).otherwise({
         templateUrl: "/partials/about.html"
       })
+
+  $locationProvider.html5Mode(true);
 })
 
 app.controller('ApplicationController', function ($scope, $location) {
-  $scope.setBackground = function () {
-    return '/' == $location.path() || '/signup' == $location.path();
-  }
+  
 });
 
-app.controller('SignupController', function ($scope, UsersService) {
+app.controller('SignupController', function ($scope, $location, $cookies, UsersService) {
+  $scope.errors = false;
   $scope.createUser = function() {
     UsersService.create($scope.newUser).then(function(response) {
+      if (response.data.error) {
+        $scope.errors = "Username already exists";
+        $location.path('/signup');
+      } else {
+        $cookies.put('user_id', response.data.id);
+        $location.path('/users/' + response.data.id);
+      }
     });
   }
 })
@@ -41,14 +53,16 @@ app.factory('UsersService', function($http) {
   var users;
   return {
     create: function(attrs) {
-      return $http.post('/api/signup', attrs).then(function(response) {
-      })
+      return $http.post('/api/signup', attrs);
     },
     all: function () {
       return $http.get('/api/users').then(function (response) {
         users = response.data
         return users;
       })
+    },
+    signin: function () {
+      return $http.get('/api/signin');
     }
   }
 })
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../lib/users');
+var validate = require('../lib/user_validation');
+var createUser = require('../lib/create_user');
 
 router.get('/users', function (req, res, next) {
   db.getUsers().then(function(users) {
@@ -9,8 +11,29 @@ router.get('/users', function (req, res, next) {
 });
 
 router.post('/signup', function (req, res, next) {
-  res.redirect('/');
+  validate.userExists(req.body.username, function (result) {
+    if(!result) {
+      createUser(req.body.username, req.body.password).then(function (user) {
+        res.json(user);
+        })
+    } else {
+      res.json({error: true});
+    }
+  });
 })
+
+router.post('/signin', function (req, res, next) {
+  validate.userExists(req.body.username, function (result) {
+    if(!result) {
+      res.json({error: "User does not exist"})
+    } else if (validate.checkPassword(req.body.password, result.hashed_pass)) {
+      res.json(result)
+    } else {
+      res.json({ error: "Passwords do not match"})
+    }
+  })
+});
+
 
 
 module.exports = router;
