c9393226595bcbe9f74f23d89e6489d5c50f7320
marthaberner <marthaberner.mb@gmail.com>
2015-09-03 19:10:22 -0600

    extract client side enable CORS

--- public/js/controllers/users_controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('UsersController', function ($scope, $cookies, UsersService) {
-  $scope.loggedInUser = $cookies.get('session_id');
-  UsersService.all().then(function (users) {
-    $scope.users = users;
-  })
-})
