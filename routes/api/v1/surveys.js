626d36170741c1af317d49d7fbd0a211fc7c54fe
marthaberner <marthaberner.mb@gmail.com>
2015-10-07 10:52:05 -0600

    resolve survey post route

--- routes/api/v1/surveys.js
+++ routes/api/v1/surveys.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
   new Survey(req.body).save().then(function (response) {
-
+    res.json(response);
   })
 })
 
