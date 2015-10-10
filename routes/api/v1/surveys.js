3131a8e743e0d9350abb8a8ae6c4cd6b66a02c35
marthaberner <marthaberner.mb@gmail.com>
2015-10-10 13:53:36 -0600

    rollback knex and bookshelf for surveys

--- routes/api/v1/surveys.js
+++ routes/api/v1/surveys.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var Survey = require('../../../models/survey');
 
 router.get('/', function (req, res, next) {
-  Survey.fetchAll().then(function (surveys) {
-    res.json(surveys)
-  })
+
 })
 
 router.post('/', function (req, res, next) {
-  new Survey(req.body).save().then(function (response) {
-    res.json(response);
-  })
+
 })
 
 router.get('/:id', function (req, res, next) {
-  return new Survey({ id: req.params.id }).fetch().then(function (response) {
-    res.json(response.attributes);
-  })
+
 })
 
 module.exports = router;
