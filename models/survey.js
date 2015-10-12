8d5849729e22973374edc5955f97d168d458ca5e
marthaberner <marthaberner.mb@gmail.com>
2015-10-12 13:02:48 -0600

    add survey item association

--- models/survey.js
+++ models/survey.js
@@ -1,7 +1,11 @@
 var bookshelf = require('../config/connection').surveys
+var SurveyItem = require('./survey_item');
 
 var Survey = bookshelf.Model.extend({
-  tableName: 'surveys'
+  tableName: 'surveys',
+  surveyItems: function() {
+    return this.hasMany(SurveyItem);
+  }
 });
 
 module.exports = Survey;
