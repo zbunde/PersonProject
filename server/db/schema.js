d078019f0a6722c9b140dcbddd977246cf1e957a
Chyld Medford <chyld.medford@gmail.com>
2015-12-09 23:09:27 -0800

    remove old seed files

--- server/db/schema.js
+++
@@ -1,9 +0,0 @@
-var Schema = {
-  users: {
-    id: {type: 'increments', nullable: false, primary: true},
-    username: {type: 'string', nullable: false, unique: true},
-    hashed_pass: {type: 'string', nullable: false}
-  }
-}
-
-module.exports = Schema;
