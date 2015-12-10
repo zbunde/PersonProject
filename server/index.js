3cb131dbf66e53b0d14ceaf62f845686e2f4ffd9
Chyld Medford <chyld.medford@gmail.com>
2015-12-09 23:36:58 -0800

    added passport

--- server/index.js
+++ server/index.js
@@ -3,14 +3,22 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 require('dotenv').load()
 
+var passport = require('passport')
+require('./config/passport')(passport);
+
 var app = express();
 var port = process.env.PORT || 3000;
 var debug = require('debug')('princeton-survey-app:server');
 
-app.use(express.static(__dirname + '/../../public'));
+app.use(express.static(__dirname + '/../public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/v1/users', require('./routes/api/v1/users')(passport));
+app.use('/api/v1/surveys', require('./routes/api/v1/surveys'));
 
 var server = app.listen(port, function(){
   debug('Listening on Port:', port)
