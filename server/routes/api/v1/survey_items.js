c93ec25888cf92a6ddb9885998cea8b5c04a00bf
Chyld Medford <chyld.medford@gmail.com>
2015-12-12 08:25:14 -0800

    denormalized the returned sql

--- server/routes/api/v1/survey_items.js
+++ server/routes/api/v1/survey_items.js
@@ -7,12 +7,25 @@ var Field = require('../../../models/field');
 
 var bookshelf = require('../../../config/connection').surveys;
 router.get('/:id', function (req, res, next){
-  bookshelf.knex.raw("select q.* from questions q inner join questions_versions qv on q.id = qv.question_id and qv.version_id = (select version from versions where survey_id = ? order by version desc limit 1) order by q.position", req.params.id).then(function(data1){
-    var ids = data1.rows.map(function(r){return r.id});
-    var qqq = ids.reduce(function(a, c){return a + ',?'}, '').slice(1);
-    bookshelf.knex.raw("select f.*, fq.* from fields f inner join fields_questions fq on f.id = fq.field_id where fq.question_id in (" + qqq + ") order by question_id, position", ids).then(function(data2){
-      res.json({questions: data1.rows, fields: data2.rows});
+  bookshelf.knex.raw("select q.id as qid, q.group as qgroup, q.position as qposition, q.text as qtext, q.dependent_id as qdependendid, f.id as fid, f.value as fvalue, f.position as fposition, f.text as ftext, f.widget as fwidget, f.metadata as fmetadata, fq.* from fields f inner join fields_questions fq on f.id = fq.field_id inner join questions q on q.id = fq.question_id where fq.question_id in (select q.id from questions q inner join questions_versions qv on q.id = qv.question_id and qv.version_id = (select version from versions where survey_id = ? order by version desc limit 1))", req.params.id).then(function(data){
+    var obj = {};
+    data.rows.forEach(function(r){
+      if(!obj[r.qid]){
+        obj[r.qid] = {};
+        obj[r.qid].question = {};
+        obj[r.qid].fields = [];
+
+        obj[r.qid].question.id = r.qid;
+        obj[r.qid].question.text = r.qtext;
+        obj[r.qid].question.position = r.qposition;
+        obj[r.qid].question.group = r.qgroup;
+        obj[r.qid].question.dependent_id = r.qdependendid;
+      }
+
+      obj[r.qid].fields.push({id: r.fid, value: r.fvalue, position: r.fposition, text: r.ftext, widget: r.fwidget, metadata: r.fmetadata});
     });
+
+    res.json({obj: obj});
   });
 });
 
