2090cf30a092fe1579110af92d900ebb1fac432e
Chyld Medford <chyld.medford@gmail.com>
2015-12-14 14:54:08 -0800

    sql query modified to send back group data

--- server/routes/api/v1/survey_items.js
+++ server/routes/api/v1/survey_items.js
@@ -32,7 +32,7 @@ router.post('/', function(req, res){
 });
 
 router.get('/:id', function (req, res){
-  bookshelf.knex.raw("select qv.version_id, q.id as qid, q.group as qgroup, q.position as qposition, q.text as qtext, q.dependent_id as qdependendid, f.id as fid, f.value as fvalue, f.position as fposition, f.text as ftext, f.widget as fwidget, f.metadata as fmetadata, fq.* from fields f inner join fields_questions fq on f.id = fq.field_id inner join questions q on q.id = fq.question_id inner join questions_versions qv on qv.question_id = q.id where fq.question_id in (select q.id from questions q inner join questions_versions qv on q.id = qv.question_id and qv.version_id = (select version from versions where survey_id = ? order by version desc limit 1))", req.params.id).then(function(data){
+  bookshelf.knex.raw("select qv.version_id, q.id as qid, q.group_number as qgroup_number, q.group_type as qgroup_type, q.group_title as qgroup_title, q.group_description as qgroup_description, q.position as qposition, q.text as qtext, q.dependent_id as qdependendid, f.id as fid, f.value as fvalue, f.position as fposition, f.text as ftext, f.widget as fwidget, f.metadata as fmetadata, fq.* from fields f inner join fields_questions fq on f.id = fq.field_id inner join questions q on q.id = fq.question_id inner join questions_versions qv on qv.question_id = q.id where fq.question_id in (select q.id from questions q inner join questions_versions qv on q.id = qv.question_id and qv.version_id = (select version from versions where survey_id = ? order by version desc limit 1))", req.params.id).then(function(data){
     var obj = {};
     data.rows.forEach(function(r){
       if(!obj[r.qid]){
@@ -45,7 +45,10 @@ router.get('/:id', function (req, res){
         obj[r.qid].question.question_id = r.qid;
         obj[r.qid].question.text = r.qtext;
         obj[r.qid].question.position = r.qposition;
-        obj[r.qid].question.group = r.qgroup;
+        obj[r.qid].question.group_number = r.qgroup_number;
+        obj[r.qid].question.group_type = r.qgroup_type;
+        obj[r.qid].question.group_title = r.qgroup_title;
+        obj[r.qid].question.group_description = r.qgroup_description;
         obj[r.qid].question.dependent_id = r.qdependendid;
       }
 
