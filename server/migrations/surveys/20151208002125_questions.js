5e63edc3d4b17f94f54b424554b72ad66ff341bf
Chyld Medford <chyld.medford@gmail.com>
2015-12-14 14:27:18 -0800

    adding group data to schema

--- server/migrations/surveys/20151208002125_questions.js
+++ server/migrations/surveys/20151208002125_questions.js
@@ -2,9 +2,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('questions', function (t) {
     t.string('id').primary();
-    t.integer('group');
-    t.integer('position');
+    t.integer('group_number');
+    t.integer('group_position');
+    t.string('group_type');
+    t.string('group_title');
+    t.text('group_description');
     t.text('text');
+    t.integer('position');
     t.string('dependent_id').references('questions.id');
     t.integer('survey_id').references('surveys.id');
     t.timestamps();
