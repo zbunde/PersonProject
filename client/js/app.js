8723382f4f67d20ee809d1af0bd3829015049484
Chyld Medford <chyld.medford@gmail.com>
2016-01-11 04:39:29 +0000

    reformat ui router code

--- client/js/app.js
+++ client/js/app.js
@@ -2,101 +2,29 @@ var app = angular.module('person-project', ['ui.router', 'ngCookies', 'angularMo
 
 app.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", "$httpProvider",
   function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
-
   $locationProvider.html5Mode(true);
-
   $httpProvider.interceptors.push("AuthInterceptor");
-
   $urlRouterProvider.otherwise('/');
+
   $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: '/partials/welcome/main.html',
-      controller: 'SurveysController'
-    })
-    .state('signup', {
-      url: '/signup',
-      templateUrl: '/partials/registrations/new.html',
-      controller: 'UsersController'
-    })
-    .state('signin', {
-      url: '/signin',
-      templateUrl: '/partials/users/signin.html',
-      controller: 'UsersController'
-    })
-    .state('surveys', {
-      url: '/surveys',
-      templateUrl: '/partials/surveys/index.html',
-      controller: 'SurveysController'
-    })
-    .state('user', {
-      url: '/users',
-      templateUrl: '/partials/users/dashboard.html',
-      controller: 'UsersController'
-    })
-    .state('user.survey', {
-      url: '/surveys/:survey_id',
-      controller: 'SurveyController',
-      views: {
-        '' : {
-          templateUrl: '/partials/surveys/show.html',
-          controller: 'SurveyController'
-        },
-        'surveyItems@user.survey' : {
-          templateUrl: 'partials/survey_items/show.html',
-          controller: 'SurveyItemController as vm'}
-      }
-    })
-    .state('user.results', {
-      url: '/results',
-      templateUrl: 'partials/users/results.html',
-      controller: 'ResultsController',
-    })
-    .state('admin', {
-      url: '/admin',
-      templateUrl: '/partials/admin/dashboard.html',
-      controller: 'AdminController'
-    })
-    .state('admin.new', {
-      url: '/new',
-      templateUrl: '/partials/admin/new.html',
-      controller: 'UsersController'
-    })
-    .state('admin.select_surveys', {
-      url: '/surveys',
-      templateUrl: 'partials/admin/select_surveys.html',
-      controller: 'AdminSelectSurveysController'
-    })
-    .state('admin.select_survey_items', {
-      url: '/surveys/download',
-      templateUrl: 'partials/admin/select_survey_items.html',
-      controller: 'AdminSelectSurveyItemsController'
-    })
-    .state('admin.new_survey', {
-      url: '/surveys/new',
-      templateUrl: '/partials/surveys/new',
-      controller: 'SurveyController as vm'
-    })
-    .state('admin.survey', {
-      url: '/surveys/:survey_id',
-      templateUrl: '/partials/surveys/show.html',
-      controller: 'SurveyController'
-    })
-    .state('admin.users', {
-      url: '/users',
-      templateUrl: '/partials/users/index.html',
-      controller: 'UsersController'
-    })
-    .state('admin.user', {
-      url: '/users/:user_id',
-      templateUrl: '/partials/admin/show_user.html',
-      controller: 'UsersController'
-    })
-    .state('terms', {
-      url: '/terms',
-      templateUrl: '/partials/welcome/terms.html',
-      controller: 'UsersController'
-    })
+  .state('home', {url: '/', templateUrl: '/partials/welcome/main.html', controller: 'SurveysController'})
+  .state('terms', {url: '/terms', templateUrl: '/partials/welcome/terms.html', controller: 'UsersController'})
+  .state('signup', {url: '/signup', templateUrl: '/partials/registrations/new.html', controller: 'UsersController'})
+  .state('signin', {url: '/signin', templateUrl: '/partials/users/signin.html', controller: 'UsersController'})
+  .state('surveys', {url: '/surveys', templateUrl: '/partials/surveys/index.html', controller: 'SurveysController'})
+  .state('user', {url: '/users', templateUrl: '/partials/users/dashboard.html', controller: 'UsersController'})
+  .state('user.survey', {url: '/surveys/:survey_id', controller: 'SurveyController',
+      views: {'' :                        {templateUrl: '/partials/surveys/show.html', controller: 'SurveyController'},
+              'surveyItems@user.survey' : {templateUrl: 'partials/survey_items/show.html', controller: 'SurveyItemController as vm'}}})
+  .state('user.results', {url: '/results', templateUrl: 'partials/users/results.html', controller: 'ResultsController',})
+  .state('admin', {url: '/admin', templateUrl: '/partials/admin/dashboard.html', controller: 'AdminController'})
+  .state('admin.new', {url: '/new', templateUrl: '/partials/admin/new.html', controller: 'UsersController'})
+  .state('admin.select_surveys', {url: '/surveys', templateUrl: 'partials/admin/select_surveys.html', controller: 'AdminSelectSurveysController'})
+  .state('admin.select_survey_items', {url: '/surveys/download', templateUrl: 'partials/admin/select_survey_items.html', controller: 'AdminSelectSurveyItemsController'})
+  .state('admin.new_survey', {url: '/surveys/new', templateUrl: '/partials/surveys/new', controller: 'SurveyController as vm'})
+  .state('admin.survey', {url: '/surveys/:survey_id', templateUrl: '/partials/surveys/show.html', controller: 'SurveyController'})
+  .state('admin.users', {url: '/users', templateUrl: '/partials/users/index.html', controller: 'UsersController'})
+  .state('admin.user', {url: '/users/:user_id', templateUrl: '/partials/admin/show_user.html', controller: 'UsersController'})
 }]);
 
 app.run(["UsersService", "$rootScope", "LocalAuthService", "$location",
