de9acba6124465b60153505604b0ee3683a4d83d
Chyld Medford <chyld.medford@gmail.com>
2016-01-19 00:33:59 +0000

    add recorded time to completions table schema

--- server/migrations/surveys/20151208002160_completions.js
+++ server/migrations/surveys/20151208002160_completions.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
     t.integer('survey_id').references('surveys.id');
     t.integer('version_id').references('versions.id');
     t.string('user_id');
+    t.integer('recorded_time'); // number of seconds to take survey
     t.timestamps();
   });
 };
